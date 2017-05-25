d6832e222f48f937d3b2ca50e6e94ec2f0f106ac
Soumya Ray <soumya.ray@gmail.com>
2017-05-25 22:41:21 +0800

    Use econfig gem instead of config_env

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
-gem 'config_env'
+gem 'puma'
+gem 'econfig'
 gem 'http'
--- app.rb
+++ app.rb
@@ -1,58 +1,68 @@
 # Classic style Ruby app demonstrating Github OAuth
-# Run using `$ ruby app.rb`
+# Run using `$ rackup -p 4567`
 
+require 'econfig'
 require 'sinatra'
-require 'config_env'
 require 'http'
 
-ConfigEnv.path_to_config("#{__dir__}/config_env.rb")
-use Rack::Session::Cookie, secret: 'sdafljk'
+# Demo app for three-legged OAuth
+class OAuthDemo < Sinatra::Base
+  extend Econfig::Shortcut
+  use Rack::Session::Cookie, secret: 'sdafljk'
 
-get '/login' do
-  "<a href='https://github.com/login/oauth/authorize?"\
-    "client_id=#{ENV['GH_CLIENT_ID']}&"\
-    "scope=user:email'> Login with Github</a>"
-end
-
-get '/callback' do
-  result = HTTP.headers(accept: 'application/json')
-               .post('https://github.com/login/oauth/access_token',
-                     form: { client_id: ENV['GH_CLIENT_ID'],
-                             client_secret: ENV['GH_CLIENT_SECRET'],
-                             code: params['code'] })
-               .parse
-
-  puts "ACCESS TOKEN: #{result}"
-
-  gh_account = HTTP.headers(user_agent: 'Config Secure',
-                            authorization: "token #{result['access_token']}",
-                            accept: 'application/json')
-                   .get('https://api.github.com/user')
-                   .parse
-
-  puts "GITHUB ACCOUNT: #{gh_account}"
-
-  session[:auth_info] = gh_account.to_json
-  redirect '/secret'
-end
-
-get '/secret' do
-  unless session[:auth_info]
-    redirect '/login'
-    halt
+  configure do
+    Econfig.env = settings.environment.to_s
+    Econfig.root = File.expand_path('.', settings.root)
   end
 
-  account = JSON.load(session[:auth_info])
+  get '/' do
+    '<a href="/secret">See the secret page</a>'
+  end
 
-  "Your best and worst friend is #{account['name']} at #{account['email']}"\
-  "<BR><a href='/logout'>logout</a>"
-end
+  get '/secret' do
+    unless session[:auth_info]
+      redirect '/login'
+      halt
+    end
 
-get '/logout' do
-  session[:auth_info] = nil
-  redirect '/'
-end
+    account = JSON.parse(session[:auth_info])
 
-get '/' do
-  '<a href="/secret">See the secret page</a>'
+    "Your best and worst friend is #{account['name']} at #{account['email']}"\
+    "<BR><a href='/logout'>logout</a>"
+  end
+
+  get '/login' do
+    url = 'https://github.com/login/oauth/authorize'
+    scope = 'user:email'
+    params = ["client_id=#{settings.config.GH_CLIENT_ID}",
+              "scope=#{scope}"]
+    "<a href='#{url}?#{params.join('&')}'> Login with Github</a>"
+  end
+
+  get '/github_callback' do
+    result = HTTP.headers(accept: 'application/json')
+                 .post('https://github.com/login/oauth/access_token',
+                       form: { client_id: settings.config.GH_CLIENT_ID,
+                               client_secret: settings.config.GH_CLIENT_SECRET,
+                               code: params['code'] })
+                 .parse
+
+    puts "ACCESS TOKEN: #{result}"
+
+    gh_account = HTTP.headers(user_agent: 'Config Secure',
+                              authorization: "token #{result['access_token']}",
+                              accept: 'application/json')
+                     .get('https://api.github.com/user')
+                     .parse
+
+    puts "GITHUB ACCOUNT: #{gh_account}"
+
+    session[:auth_info] = gh_account.to_json
+    redirect '/secret'
+  end
+
+  get '/logout' do
+    session[:auth_info] = nil
+    redirect '/'
+  end
 end
